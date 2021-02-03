4acf67977c0545f25022a516ec854d22afd54abd
Brian McRae <brian.mcrae@red-gate.com>
2021-02-03 13:54:20 -0600

    Created database and loaded initial data

--- EmployeeDB_schemadef.sql
+++ EmployeeDB_schemadef.sql
@@ -1,141 +1,53 @@
-
-
-CREATE TABLE public.departments (
-    dept_no character varying(4) NOT NULL,
-    dept_name character varying(40) NOT NULL
+CREATE TABLE departments (
+     dept_no VARCHAR(4) NOT NULL,
+     dept_name VARCHAR(40) NOT NULL,
+     PRIMARY KEY (dept_no),
+     UNIQUE (dept_name)
 );
 
-CREATE TABLE public.dept_emp (
-    emp_no integer NOT NULL,
-    dept_no character varying NOT NULL,
-    from_date date NOT NULL,
-    to_date date NOT NULL
+CREATE TABLE employees (
+	 emp_no INT NOT NULL,
+     birth_date DATE NOT NULL,
+     first_name VARCHAR NOT NULL,
+     last_name VARCHAR NOT NULL,
+     gender VARCHAR NOT NULL,
+     hire_date DATE NOT NULL,
+     PRIMARY KEY (emp_no)
 );
 
-CREATE TABLE public.dept_manager (
-    dept_no character varying(4) NOT NULL,
-    emp_no integer NOT NULL,
-    from_date date NOT NULL,
-    to_date date NOT NULL
+CREATE TABLE dept_manager (
+	dept_no VARCHAR(4) NOT NULL,
+    emp_no INT NOT NULL,
+    from_date DATE NOT NULL,
+    to_date DATE NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+   	PRIMARY KEY (emp_no, dept_no)
 );
 
-CREATE TABLE public.employees (
-    emp_no integer NOT NULL,
-    birth_date date NOT NULL,
-    first_name character varying NOT NULL,
-    last_name character varying NOT NULL,
-    gender character varying NOT NULL,
-    hire_date date NOT NULL
+CREATE TABLE salaries (
+  emp_no INT NOT NULL,
+  salary INT NOT NULL,
+  from_date DATE NOT NULL,
+  to_date DATE NOT NULL,
+  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+  PRIMARY KEY (emp_no)
 );
 
-CREATE TABLE public.salaries (
-    emp_no integer NOT NULL,
-    salary integer NOT NULL,
-    from_date date NOT NULL,
-    to_date date NOT NULL
+CREATE TABLE dept_employee (
+    emp_no INT NOT NULL,
+	dept_no VARCHAR(4) NOT NULL,
+    from_date DATE NOT NULL,
+    to_date DATE NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+   	PRIMARY KEY (emp_no, dept_no)
 );
 
-CREATE TABLE public.titles (
-    emp_no integer NOT NULL,
-    title character varying NOT NULL,
-    from_date date NOT NULL,
-    to_date date NOT NULL
+CREATE TABLE titles (
+  emp_no INT NOT NULL,
+  title VARCHAR NOT NULL,
+  from_date DATE NOT NULL,
+  to_date DATE NOT NULL,
+  FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
 );
-
-
---
--- TOC entry 2707 (class 2606 OID 16400)
--- Name: departments departments_dept_name_key; Type: CONSTRAINT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.departments
-    ADD CONSTRAINT departments_dept_name_key UNIQUE (dept_name);
-
-
---
--- TOC entry 2709 (class 2606 OID 16398)
--- Name: departments departments_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.departments
-    ADD CONSTRAINT departments_pkey PRIMARY KEY (dept_no);
-
-
---
--- TOC entry 2713 (class 2606 OID 16495)
--- Name: dept_manager dept_manager_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.dept_manager
-    ADD CONSTRAINT dept_manager_pkey PRIMARY KEY (emp_no, dept_no);
-
-
---
--- TOC entry 2711 (class 2606 OID 16490)
--- Name: employees employees_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.employees
-    ADD CONSTRAINT employees_pkey PRIMARY KEY (emp_no);
-
-
---
--- TOC entry 2715 (class 2606 OID 16510)
--- Name: salaries salaries_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.salaries
-    ADD CONSTRAINT salaries_pkey PRIMARY KEY (emp_no);
-
-
---
--- TOC entry 2719 (class 2606 OID 16535)
--- Name: dept_emp dept_emp_emp_no_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.dept_emp
-    ADD CONSTRAINT dept_emp_emp_no_fkey FOREIGN KEY (emp_no) REFERENCES public.employees(emp_no);
-
-
---
--- TOC entry 2717 (class 2606 OID 16501)
--- Name: dept_manager dept_manager_dept_no_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.dept_manager
-    ADD CONSTRAINT dept_manager_dept_no_fkey FOREIGN KEY (dept_no) REFERENCES public.departments(dept_no);
-
-
---
--- TOC entry 2716 (class 2606 OID 16496)
--- Name: dept_manager dept_manager_emp_no_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.dept_manager
-    ADD CONSTRAINT dept_manager_emp_no_fkey FOREIGN KEY (emp_no) REFERENCES public.employees(emp_no);
-
-
---
--- TOC entry 2718 (class 2606 OID 16511)
--- Name: salaries salaries_emp_no_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.salaries
-    ADD CONSTRAINT salaries_emp_no_fkey FOREIGN KEY (emp_no) REFERENCES public.employees(emp_no);
-
-
---
--- TOC entry 2720 (class 2606 OID 16546)
--- Name: titles titles_emp_no_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.titles
-    ADD CONSTRAINT titles_emp_no_fkey FOREIGN KEY (emp_no) REFERENCES public.employees(emp_no);
-
-
--- Completed on 2021-02-02 10:18:08
-
---
--- PostgreSQL database dump complete
---
-
